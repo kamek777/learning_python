dc60bb92ab393c1005409c0c26045e339b94d7b3
Kamil <116445995+kamek777@users.noreply.github.com>
2023-03-25 08:06:20 +0100

    Add files via upload

---
+++ projekty_pythona/exercises_functions.py
@@ -0,0 +1,15 @@
+# Zrób to sam
+
+# Ćw 8.1 Komunikat
+
+def display_messages():
+    print("W tym rozdziale uczę się funkcji w pythonie!")
+
+display_messages()    
+
+# Ćw 8.2 Ulubiona książka
+
+def favourite_book(title):
+    print(f"Jedną z moich ulubionych książek jest {title.title()}!")
+    
+favourite_book("harry potter")    
