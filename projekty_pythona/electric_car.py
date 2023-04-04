86b4d11d938264d5e3e08a87077c9bd1fe3e711c
Kamil <116445995+kamek777@users.noreply.github.com>
2023-04-04 17:50:41 +0200

    Add files via upload

--- projekty_pythona/electric_car.py
+++ projekty_pythona/electric_car.py
@@ -26,15 +26,49 @@ class Car():
     def increment_odometer(self,kilometers):
         self.odometer_reading += kilometers
         
+class Battery():
+    """Prosta próba modelowania akumulatora samochodu elektrycznego."""
+    
+    def __init__(self,battery_size=75):
+        """Inicjalizacja atrybutów akumulatora."""
+        self.battery_size = battery_size
+        
+    def describe_battery(self):
+        """Wyświetlenie informacji o wielkości akumulatora."""
+        print(f"Ten samochód ma akumulator o pojemności {self.battery_size} kWm.")          
+    
+    def get_range(self):
+        """Wyświetla informacje o zasięgu samochodu na podstawie pojemności akumulatora."""
+        if self.battery_size == 75:
+            range = 260
+        elif self.battery_size == 100:
+            range = 315
+            
+        print(f"Zasięg tego samochodu wynosi około {range} km po pełnym naładowaniu akumulatora.")   
+
 # Klasa potomna (podrzędna) 
 class ElectricCar(Car):
     """Przedstawia cechy charakterystyczne samochodu elektrycznego."""
     
     def __init__(self,make,model,year):
-        """Inicjalizacja atrybutów klasy nadrzędnej."""
+        """Inicjalizacja atrybutów klasy nadrzędnej oraz dodanie nowych atrybutów do klasy potomnej."""
         super().__init__(make,model,year)
+        self.battery = Battery()   
+
+    def describe_battery(self):
+        """Wyświetla informacje o wielkości akumulatora."""
+        print(f"Ten samochod ma akumulator o pojemności {self.battery_size} kWh.")
+     
+    def fill_gas_tank():
+        """Samochód o napędzie elektrycznym nie ma zbiornika paliwa."""
+        print("Ten samochód nie wymaga tankowania paliwa!")    
+        
 
 # Funkcja super() umożliwia połączenie między klasą nadrzedną a potomną!
 
 my_tesla = ElectricCar('tesla','model s', 2020)
-print(my_tesla.get_descriptive_name())                               
+print(my_tesla.get_descriptive_name())  
+my_tesla.battery.describe_battery()   
+my_tesla.battery.get_range()      
+
+            
