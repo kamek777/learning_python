b6238c4a2b5e2f1ebff1815afb82cc3548857dae
Kamil <116445995+kamek777@users.noreply.github.com>
2023-04-15 21:21:28 +0200

    Delete greet_user.py

--- greet_user.py
+++
@@ -1,8 +0,0 @@
-# Program, witający użytkownika zapisanego w pliku JSON (wczytując go).
-
-import json
-filename = 'username.json'
-
-with open(filename) as f:
-    username = json.load(f)
-    print(f"Witamy ponownie, {username}!")
