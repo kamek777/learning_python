3f3f766cafc28910aa5313ebc808d94c9c0e22da
Kamil <116445995+kamek777@users.noreply.github.com>
2023-04-18 19:46:54 +0200

    Add files via upload

--- projekty_pythona/name_function.py
+++ projekty_pythona/name_function.py
@@ -1,6 +1,10 @@
 # Testowanie funkcji przy pomocy modułu unittest.
-def get_formatted_name(first,last):
+def get_formatted_name(first,last,middle=''):
     """Generuje sformatowane pełne imię i nazwisko użytkownika."""
-    full_name = f"{first} {last}"
+    if middle:
+        full_name = f"{first} {middle} {last}"
+    else:
+        full_name = f"{first} {last}"
     return full_name.title()
     
+print(get_formatted_name('jan','sebastian','kowalski'))
--- projekty_pythona/test_name_function.py
+++ projekty_pythona/test_name_function.py
@@ -6,10 +6,16 @@ from name_function import get_formatted_name
 class NamesTestCase(unittest.TestCase):
     """Testy dla programu 'name_function.py'."""
     
+    
     def test_first_last_name(self):
-        """Czy dane w postaci 'Janis Joplin' są obsługiwane prawidłowo?"""
-        formatted_name = get_formatted_name('jan', 'kowalski')
+        """Czy dane w postaci 'Jan Kowalski' są obsługiwane prawidłowo?"""
+        formatted_name = get_formatted_name('jan','kowalski')
         self.assertEqual(formatted_name, 'Jan Kowalski')
+        
+    def test_first_last_middle_name(self):
+        """Czy dane w postaci 'Jan Sebastian Kowalski' są obsługiwane prawidłowo?"""
+        formatted_name = get_formatted_name('jan','kowalski','sebastian')
+        self.assertEqual(formatted_name, 'Jan Sebastian Kowalski')
     
 if __name__ == '__main__':
     unittest.main()
