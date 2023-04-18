272f7e9e87f34b5ec47d2a9fa2ad39f148fe87bd
Kamil <116445995+kamek777@users.noreply.github.com>
2023-04-18 18:35:00 +0200

    Add files via upload

---
+++ projekty_pythona/name_function.py
@@ -0,0 +1,6 @@
+# Testowanie funkcji przy pomocy modułu unittest.
+def get_formatted_name(first,last):
+    """Generuje sformatowane pełne imię i nazwisko użytkownika."""
+    full_name = f"{first} {last}"
+    return full_name.title()
+    
