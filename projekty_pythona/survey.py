d180b55dfbf9cb8b1c9b0ae55b4983510657ba2b
Kamil <116445995+kamek777@users.noreply.github.com>
2023-04-19 15:56:19 +0200

    Add files via upload

---
+++ projekty_pythona/survey.py
@@ -0,0 +1,30 @@
+# Testowanie klasy 
+
+# Metody asercji oferowane przez moduł unittest:
+# assertEqual(a,b) ---> Sprawdza czy a == b.
+# assertNotEqual(a,b) ---> Sprawdza, czy a != b.
+# assertTrue(x) ---> Sprawdza, czy x przyjmuje wartość True.
+# assertFalse(x) ---> Sprawdza, czy x przyjmuje wartość False.
+# assertIn(element,lista) ---> Sprawdza, czy element jest na liście.
+# assertNotIn(element,lista) ---> Sprawdza, czy element nie znajduje się na liście.
+
+class AnonymousSurvey():
+    """Przechowuje anonimowe odpowiedzi na pytania w ankiecie."""
+    
+    def __init__(self,question):
+        """Przechowuje pytanie i przygotowuje do przechowywania odpowiedzi."""
+        self.question = question
+        self.responses = []
+        
+    def show_question(self):
+        """Wyświetla pytanie z ankiety."""
+        print(self.question)
+    
+    def store_response(self,new_response):
+        """Przechowuje pojedynczą odpowiedź na pytanie z ankiety."""
+        
+    def show_results(self):
+        """Wyświetla wszystkie udzielone odpowiedzi."""
+        print("Oto wyniki ankiety: ")
+        for response in self.responses:
+            print(f"- {response}")
