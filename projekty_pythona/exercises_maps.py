0183a1499845a875f1c4e2ae5fa5388716ad027f
kamil_7 <kamil.kwiek@student.pk.edu.pl>
2023-06-02 00:07:47 +0200

    Poprawka

--- projekty_pythona/exercises_maps.py
+++ projekty_pythona/exercises_maps.py
@@ -8,7 +8,7 @@ x_values = range(50001)
 y_values = [x**3 for x in x_values]
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, c = y_values, cmap=plt.cm.Reds, s=10)
+ax.scatter(x_values, y_values, c = y_values, cmap= plt.cm.Reds, s=10)
 
 #Zdefiniowanie tytułu wykresu i etykiet osi.
 ax.set_title("Sześciany liczb", fontsize=24)
