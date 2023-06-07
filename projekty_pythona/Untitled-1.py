769e8048f03718298b67f75a9189242777b7d194
kamil_7 <kamil.kwiek@student.pk.edu.pl>
2023-06-07 10:58:45 +0200

    Adding one file

---
+++ projekty_pythona/Untitled-1.py
@@ -0,0 +1,12 @@
+def divided_by_five(lista):
+    count = 0 
+    for element in lista:
+        if element % 5 == 0 and element < 100: 
+            count += 1
+        elif element > 100:
+            return 0
+    return count
+        
+    
+nowa_lista = [5,10,23,33,99]
+print(divided_by_five(nowa_lista))
