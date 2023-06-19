095b98d60cb3950ed150b82ce79a062a868c4c0f
kamil_7 <kamil.kwiek@student.pk.edu.pl>
2023-06-19 16:49:00 +0200

    Poprawienie

--- projekty_pythona/rw_visual.py
+++ projekty_pythona/rw_visual.py
@@ -2,16 +2,13 @@ import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
 #Przygotowaie danych błądzenia losowego i wyświetlenie wykresu z punktami.
-
 rw = RandomWalk()
 rw.fill_walk()
 
 #Wyświetlenie punktów błądzenia losowego.
-
 plt.style.use('classic')
 fig, ax = plt.subplots()
 ax.scatter(rw.x_values, rw.y_values, s=15)
 
 #Wyświetlenie wykresu
-
 plt.show()
