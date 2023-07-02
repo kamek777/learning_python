c4ccc4fb4e7489502df0068b3fab11b55df8dad2
kamil_7 <kamil.kwiek@student.pk.edu.pl>
2023-07-02 13:24:36 +0200

    Poprawka

--- projekty_pythona/eq_explore_data.py
+++ projekty_pythona/eq_explore_data.py
@@ -1,7 +1,6 @@
 import json
 
 #Analiza struktury danych
-
 filename = 'D:/repo_learning/learning_python/projekty_pythona/eq_explore_data.py/username.json'
 with open(filename) as f:
     all_eq_data = json.load(f)
---
+++ projekty_pythona/new_file.py
@@ -0,0 +1,8 @@
+n = 1
+
+for i in range(4):
+    for j in range(i+1):
+        print(n+1, str(i)) 
+      
+    
+        
