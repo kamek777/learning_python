ef4763c1e2b6333b9796709eae9db909d5dae770
kamil_7 <kamil.kwiek@student.pk.edu.pl>
2023-07-17 11:07:20 +0200

    Poprawka

--- projekty_pythona/person.py
+++ projekty_pythona/person.py
@@ -9,7 +9,6 @@ def build_person(first_name,last_name):
 print(build_person('michael','jordan'))
 
 # Modyfikacja funkcji o wiek, wartość None nie przypisuje żadnej konkretnej wartości
-
 def build_person1(first_name,last_name,age=None):
     """ Zwraca słownik informacji o danej osobie"""
     person = {'first': first_name.title(), 'last': last_name.title()}
