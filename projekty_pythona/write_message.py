214599776e06cf0e628821992d0de262a5f4ebfa
kamil_7 <kamil.kwiek@student.pk.edu.pl>
2023-08-27 00:15:55 +0200

    Poprawka

--- projekty_pythona/write_message.py
+++ projekty_pythona/write_message.py
@@ -5,4 +5,5 @@ with open(filename, 'a') as file_object:
     file_object.write("Uwielbiam programowac!\n")
     file_object.write("Zwlaszcza w pyhonie.\n")
     file_object.write("Uwielbiam odnajdywac elementy w ogromnych zbiorach danych.")
+
 # 'w' nadpisuje ciąg znaków, 'a' dodaje kolejne bez usunięcia poprzednich ciągów    
