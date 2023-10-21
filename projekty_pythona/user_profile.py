71f5ea6fc8744f1e39bdc7cd9126938898239f18
kamil_7 <kamil.kwiek@student.pk.edu.pl>
2023-10-21 19:58:36 +0200

    Poprawka.

--- projekty_pythona/user_profile.py
+++ projekty_pythona/user_profile.py
@@ -6,6 +6,5 @@ def build_profile(first, last, **user_info):
     return user_info
 
 user_profile = build_profile('albert','einstein', location = 'princeton', field = 'fizyka')
-
 print(user_profile)
 #**kwargs -  oznacza utworzenie pustego słownika z parami klucz wartość w funkcji    
