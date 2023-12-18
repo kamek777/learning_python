13d96a2d4085229fa165a111ff0442a66a44f253
kamil_7 <kamil.kwiek@student.pk.edu.pl>
2023-12-18 21:50:25 +0100

    Poprawka

--- projekty_pythona/printing_models.py
+++ projekty_pythona/printing_models.py
@@ -8,7 +8,6 @@ from printing_functions import print_models as pm, show_completed_models as scm
 # Rozpoczynamy od pewnych projektów, które mają być wydrukowane.
 unprinted_designs = ['etui telefonu', 'robot pendant', 'dwunastościan']
 completed_models = []
-
 # Wywołanie tych funkcji:
 pm(unprinted_designs,completed_models)
 scm(completed_models)
